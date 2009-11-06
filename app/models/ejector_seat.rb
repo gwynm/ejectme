b12a4962151c24ef8bb9f918ef0f5969193b4a64
Gwyn Morfey <gwyn@Unknown-00-23-6c-89-83-be.config>
2009-11-06 14:24:45 +0000

    Scraper works, sort of

--- app/models/ejector_seat.rb
+++ app/models/ejector_seat.rb
@@ -3,11 +3,12 @@ class EjectorSeat
   #Find a suitable flight pair, or return nil if there's nothing
   def self.eject
     nil
+    #date_pairs = self.date_pairs_for_ejection
   end
   
 private
   #return a flight object, or nil
-  def self.find_flight_for_date(date)
+  def self.find_flight_for_date(from_string,to_string,date)
   end
   
 end
