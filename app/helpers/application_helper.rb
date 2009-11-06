bc51752ac3b2d094e3e272f1c023465e035c294e
Gwyn Morfey <gwyn@Unknown-00-23-6c-89-83-be.config>
2009-11-06 16:47:54 +0000

    Fixed return flight bug?

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def wikipedia_link(str)
+    link_to str,"http://en.wikipedia.org/wiki/Special:Search/" + str + "_" + "airport"
+  end
 end
--- lib/ryanair_scraper.rb
+++ lib/ryanair_scraper.rb
@@ -1,43 +1,35 @@
 class RyanairScraper < Scraper
   
-  def self.rebuild_flights
+  def self.build_all_flights
     from_airport = "STN"
-    to_airports = %w(ORK NRN PIK NOC LRH MRS PMF PEG PIS NYO)
+    to_airports = %w(RYG ORK NRN NOC LRH MRS PMF PEG PIS NYO)
 
     to_airports.each do |to_airport|
-      upcoming_outgoing_dates.each do |date|
-        price = self.price_for_flight(date,from_airport,to_airport)
-        puts "#{from_airport} -> #{to_airport} on #{date.to_s}: GBP #{price}"
-        Flight.create!(:from_airport=>from_airport,:to_airport=>to_airport,:departs_at=>date,:price=>price) if price
-      end
-      upcoming_incoming_dates.each do |date|
-        price = self.price_for_flight(date,to_airport,from_airport)
-        puts "#{to_airport} -> #{from_airport} on #{date.to_s}: GBP #{price}"
-        Flight.create!(:from_airport=>to_airport,:to_airport=>from_airport,:departs_at=>date,:price=>price) if price
+      upcoming_dates.each do |date|
+        self.build_flights(date,from_airport,to_airport)
+        puts "#{from_airport} -> #{to_airport} on #{date.to_s}"
       end
     end
   end
   
-  def self.upcoming_outgoing_dates
-    (1..30).collect{|n| n.days.from_now}.select{|d| %w(Fri Sat).include?(d.strftime("%a"))}.compact
-  end
-  def self.upcoming_incoming_dates
-    (3..32).collect{|n| n.days.from_now}.select{|d| %w(Sun Mon).include?(d.strftime("%a"))}.compact
+  def self.upcoming_dates
+    (1..30).collect{|n| n.days.from_now}.select{|d| %w(Fri Sat Sun Mon).include?(d.strftime("%a"))}.compact
   end
     
   #Query ryanair and create a Flight object 
-  def self.price_for_flight(date,from_string,to_string)
-    prices = []
+  def self.build_flights(date,from_string,to_string)
     agent = WWW::Mechanize.new
-   # agent.set_proxy("127.0.0.1", 8888)
+    #agent.set_proxy("127.0.0.1", 8888)
     page = agent.get('http://ryanair.com/php/sbforms/form.php?val=GB')
     booking_form = page.forms.first
     booking_form.sector1_o = "a" + from_string
     booking_form.sector1_d = to_string
     booking_form.delete_field!("SearchBy")
     booking_form.date1 = date.strftime("%Y%m%d")
+    booking_form.date2 = date.strftime("%Y%m%d")
     booking_form.m1 = date.strftime("%Y%m%d") + "a" + from_string + to_string
-    booking_form.nom = "1"
+    booking_form.m1 = date.strftime("%Y%m%d") + to_string + "a" + from_string
+    booking_form.nom = "2"
     booking_form.pM = "0"
     booking_form.tc = "1"
     booking_form.pT = "1ADULT"
@@ -49,31 +41,41 @@ class RyanairScraper < Scraper
     results_page = agent.submit(booking_form)
     
     return nil unless results_page.root.to_s.index("Regular") #no flights on this date
-    
+        
     #now break out the flight keys
-    keys = self.get_flight_keys(results_page)
+    outgoing_keys = self.get_keys(results_page,"AvailabilityInputFRSelectView$market1")
+    incoming_keys = self.get_keys(results_page,"AvailabilityInputFRSelectView$market2")
 
-    #make a request for each one
-    keys.each do |key|
-      detail = agent.get("http://www.bookryanair.com/skysales/FRTaxAndFeeInclusiveDisplay-resource.aspx?flightKeys=#{key}&numberOfMarkets=1&keyDelimeter=+++")
-      prices << self.get_price_from_detail_page(detail)
-    end
-    
-    prices.compact.sort.first
+    #get the prices
+    best_outgoing_price = self.best_price(agent,outgoing_keys)
+    best_incoming_price = self.best_price(agent,incoming_keys)
+        
+    #later, get date from the key
+    Flight.create!(:from_airport=>from_string,:to_airport=>to_string,:departs_at=>date,:price=>best_outgoing_price) if best_outgoing_price
+    Flight.create!(:from_airport=>to_string,:to_airport=>from_string,:departs_at=>date,:price=>best_incoming_price) if best_incoming_price
   end
   
 private
 
-  def self.get_flight_keys(results_page)
+  def self.best_price(agent,keys)
+    prices = []
+    keys.each do |key|
+      detail = agent.get("http://www.bookryanair.com/skysales/FRTaxAndFeeInclusiveDisplay-resource.aspx?flightKeys=#{key}&numberOfMarkets=1&keyDelimeter=+++")
+      prices << self.get_price_from_detail_page(detail)
+    end
+    prices.compact.sort.uniq.first
+  end
+
+  def self.get_keys(results_page,v)
     keys = []
     results_page.search("//input").each do |inp|
-      if inp.attributes["name"].to_s.strip == "AvailabilityInputFRSelectView$market2"
+      if inp.attributes["name"].to_s.strip == v
         keys << inp.attributes["value"].to_s.strip
       end
     end
     keys
   end
-  
+    
   def self.get_price_from_detail_page(detail_page)
     detail_page.search("//td")[-2].children.to_s.to_f 
   end
